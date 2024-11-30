688313de3b55c70e99af0c08e8511ad4110736f6
Jacob Atanacio <jacobatanacio@gmail.com>
2024-11-29 21:59:28 -0800

    added an interface

--- GUI/dash.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-root = Tk()
-root.title('Student Management System')
-# Widgets
-w = Label(root, text = "Welcome To Class!")
-w.pack()
-root.mainloop()
--- main.py
+++ main.py
@@ -1,54 +1,135 @@
-"""
-TODO:
-figure out how to create CSV to take from both studetns and assignemts, CSV saves all assignments and student data
-Finish management system
-make students inherit management system
-make assignemnts and students associate with gradebook class
-"""
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
 
-class Management_System:
-    
-    def __init__(self, instructor, course):
-        self.students = {}
-        self.instructor = instructor
-        self.course = course
-        
-    def add_student(self, name, student_ID, email):
-        self.students[student_ID] = {"name": name, "ID": student_ID, "Email": email}
-    
-    def getCourseInfo(self):
-        return {
-            'Course': self.course,
-            'Instructor': self.instructor,
-            'Students': self.students,
-        }
+class Student:
+    def __init__(self, student_id, name, email, total_points=100):
+        self.student_id = student_id
+        self.name = name
+        self.email = email
+        self.total_points = total_points
+        self.assignments = {}
 
-class Student(Management_System):
-    def __init__():
-        pass
-    
-    def getStudentInfo(self, student_ID):
-        return self.students.get(student_ID)
-    
+    def add_assignment(self, assignment, score):
+        self.assignments[assignment.title] = score
+
+    def get_final_grade(self):
+        if not self.assignments:
+            return 0
+        return (sum(self.assignments.values()) / self.total_points) * 100
+
+    def get_letter_grade(self):
+        final_grade = self.get_final_grade()
+        if final_grade >= 90:
+            return 'A'
+        elif final_grade >= 80:
+            return 'B'
+        elif final_grade >= 70:
+            return 'C'
+        elif final_grade >= 60:
+            return 'D'
+        else:
+            return 'F'
+
 
 class Assignment:
-    def __init__(self, title, points, category, due):
+    def __init__(self, title, points, category, due_date):
         self.title = title
         self.points = points
         self.category = category
-        self.due = due
-        self.update_CSV()
-        
-    def update_CSV(self):
-        pass        
+        self.due_date = due_date
 
 
-cs22b = Management_System("AC", "CS 22B Python for Data Analytics")
-cs22b.add_student("Jacob A", "001", "jatanacio@sjsu.edu")
+class Gradebook:
+    def __init__(self):
+        self.students = {}
+        self.assignments = []
 
-print(cs22b.getCourseInfo())
+    def add_student(self, student):
+        self.students[student.student_id] = student
 
-studentInfo = Student(cs22b)
+    def add_assignment(self, assignment):
+        self.assignments.append(assignment)
+
+    def assign_grade(self, student_id, assignment, score):
+        student = self.students.get(student_id)
+        if student:
+            student.add_assignment(assignment, score)
+            self.save_to_csv()
+
+    def save_to_csv(self):
+        # Save all student grades and assignments to CSV
+        data = []
+        for student in self.students.values():
+            row = [student.name, student.student_id, student.email]
+            row.extend([student.assignments.get(assignment.title, 0) for assignment in self.assignments])
+            row.append(student.get_final_grade())
+            row.append(student.get_letter_grade())
+            data.append(row)
+
+        df = pd.DataFrame(data, columns=['Name', 'ID', 'Email'] + [a.title for a in self.assignments] + ['Final Grade', 'Letter Grade'])
+        df.set_index('Name', inplace=True)
+        df.to_csv("gradebook.csv")
+
+    def plot_final_grades(self):
+        final_grades = {student.name: student.get_final_grade() for student in self.students.values()}
+        letter_grades = {student.name: student.get_letter_grade() for student in self.students.values()}
+
+        graph = plt.bar(final_grades.keys(), final_grades.values(), color='blue')
+        for x in graph:
+            height = x.get_height()
+            plt.text(x.get_x() + x.get_width() / 2, height, f'{height:.2f}', ha='center', va='bottom', fontsize=10, color='black')
+
+        plt.xlabel('Students')
+        plt.ylabel('Final Grade')
+        plt.title('Final Grades of Students')
+        plt.show()
+
+
+class ProjectTeams:
+    def __init__(self, title, members=""):
+        self.title = title
+        self.members = members  # Now a single string to hold all members.
+
+    def add_member(self, student_name):
+        """Add a new member to the project as a string."""
+        if self.members:
+            self.members += ", " + student_name  # Append new member with a comma
+        else:
+            self.members = student_name  # First member, no comma needed
+
+    def get_team_info(self):
+        return {'Title': self.title, 'Members': self.members}
+
+"""
+Example
+student1 = Student(1, 'Fatema', 300)
+student2 = Student(2, 'Jacob', 300)
+
+assignment1 = Assignment('Midetm 1', 100, 'Exams', '1/1/2024')
+assignment2 = Assignment('Hands on 1', 100, 'HW', '1/2/2024')
+assignment3 = Assignment('Project Proposal', 100, 'Project', '1/3/2024')
+
+gradebook = Gradebook()
+gradebook.add_student(student1)
+gradebook.add_student(student2)
+
+gradebook.add_assignment(assignment1)
+gradebook.add_assignment(assignment2)
+gradebook.add_assignment(assignment3)
+
+radebook.add_grades(student1, assignment1, 85)
+gradebook.add_grades(student1, assignment2, 92)
+gradebook.add_grades(student1, assignment3, 78)
+
+gradebook.add_grades(student2, assignment1, 90)
+gradebook.add_grades(student2, assignment2, 88)
+gradebook.add_grades(student2, assignment3, 91)
+
+gradebook.create_csv()
+
+gradebook.plot_final_grades()
+"""
 
 
 
